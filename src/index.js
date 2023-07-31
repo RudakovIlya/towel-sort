7153c406611317f538741e66ef7d356ef9e13086
Ilya Rudakov <96202997+RudakovIlya@users.noreply.github.com>
2023-07-31 16:39:55 +0300

    feat: added solution towel-sort task

--- src/index.js
+++ src/index.js
@@ -1,6 +1,23 @@
-
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
-}
+const compareNumbersByAsc = (a, b) => {
+    return a - b;
+};
+
+const compareNumbersByDesc = (a, b) => {
+    return b - a;
+};
+
+module.exports = function towelSort(matrix) {
+    if (!matrix) {
+        return [];
+    }
+    return matrix.reduce((resultArray, item, index) => {
+        const currentArray =
+            index % 2 === 0
+                ? item.sort(compareNumbersByAsc)
+                : item.sort(compareNumbersByDesc);
+        resultArray.push(...currentArray);
+        return resultArray;
+    }, []);
+};
